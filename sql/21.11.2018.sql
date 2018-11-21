cce1c6b106bb030110ca4c00ec868d7ea8b36a2c
Aathil Ahamed <2017is003@stu.ucsc.cmb.ac.lk>
2018-11-21 23:13:16 +0530

    sql

--- sql/21.11.2018.sql
+++ sql/21.11.2018.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 20, 2018 at 02:21 AM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Generation Time: Nov 21, 2018 at 06:05 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,8 +17,29 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ceylon-beverages`
+-- Database: `jobs`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `categorytypeID` int(11) NOT NULL,
+  `categorytype` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`categorytypeID`, `categorytype`) VALUES
+(0, ''),
+(0, 'Accounting / Finance'),
+(1, 'Advertising / PR / MR / Event Management'),
+(3, 'Animation / Gaming');
 
 -- --------------------------------------------------------
 
@@ -51,27 +72,21 @@ INSERT INTO `orders` (`orderID`, `customerID`, `vendorID`, `productID`, `product
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Table structure for table `sector`
 --
 
-CREATE TABLE `products` (
-  `productID` int(11) NOT NULL,
-  `vendorID` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `volume` varchar(25) NOT NULL,
-  `imageName` text NOT NULL,
-  `price` varchar(25) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE `sector` (
+  `SectortypeID` int(2) NOT NULL,
+  `Sectortype` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `products`
+-- Dumping data for table `sector`
 --
 
-INSERT INTO `products` (`productID`, `vendorID`, `name`, `volume`, `imageName`, `price`) VALUES
-(1, 27, 'Coca Cola', '500ml', 'coke-500.png', 'Rs. 100'),
-(11, 28, 'Fanta', '2000ml', 'fanta-2000.png', 'Rs. 200'),
-(10, 28, '7UP', '1000ml', '7up-1000.png', 'Rs. 100'),
-(12, 27, 'Pepsi', '500ml', 'pepsi-500.png', 'Rs. 50');
+INSERT INTO `sector` (`SectortypeID`, `Sectortype`) VALUES
+(1, 'Government'),
+(2, 'Private');
 
 -- --------------------------------------------------------
 
@@ -93,12 +108,30 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userID`, `name`, `email`, `password`, `usertype`, `contact`) VALUES
-(25, 'admin', 'admin@admin.com', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'a', '123'),
-(26, 'Kasun', 'customer@customer.com', 'b39f008e318efd2bb988d724a161b61c6909677f', 'c', '456'),
-(27, 'Nimal Traders', 'vendor@vendor.com', '9fdcb2f441fcdd2e24e21bf8d45413ae72c0443c', 'v', '789'),
-(28, 'John Drinks', 'vendor2@vendor2.com', '9d737ae503ed092cd1b5e2fec61d8474be13a9c1', 'v', '7711111112'),
-(29, 'Customer 2', 'customer2@customer2.com', '80dae43ddfcfbd7a5e75b83260eab8fd35fd6778', 'c', '456'),
-(30, 'UCSC', 'ucsc', 'c0d0cb34565fe05ca2a14e8b13285bf6dbdf6dfc', 'a', '0771234567');
+(25, 'admin', 'admin@admin.com', 'd033e22ae348aeb5660fc2140aec35850c4da997', '0', '123'),
+(26, 'Kasun', 'customer@customer.com', 'b39f008e318efd2bb988d724a161b61c6909677f', '1', '456'),
+(27, 'Nimal Traders', 'vendor@vendor.com', '9fdcb2f441fcdd2e24e21bf8d45413ae72c0443c', '2', '789'),
+(28, 'John Drinks', 'vendor2@vendor2.com', '9d737ae503ed092cd1b5e2fec61d8474be13a9c1', '2', '7711111112'),
+(29, 'Customer 2', 'customer2@customer2.com', '80dae43ddfcfbd7a5e75b83260eab8fd35fd6778', '1', '456'),
+(30, 'UCSC', 'ucsc', 'c0d0cb34565fe05ca2a14e8b13285bf6dbdf6dfc', '0', '0771234567');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vacancies`
+--
+
+CREATE TABLE `vacancies` (
+  `JobID` int(11) NOT NULL,
+  `Providername` varchar(100) NOT NULL,
+  `Jobtitle` varchar(100) NOT NULL,
+  `description` varchar(25) NOT NULL,
+  `Sectortype` varchar(255) NOT NULL,
+  `ClosingDate` date NOT NULL,
+  `Contact` text NOT NULL,
+  `email` text NOT NULL,
+  `website` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -111,16 +144,16 @@ ALTER TABLE `orders`
   ADD PRIMARY KEY (`orderID`);
 
 --
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`productID`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`userID`);
+
+--
+-- Indexes for table `vacancies`
+--
+ALTER TABLE `vacancies`
+  ADD PRIMARY KEY (`JobID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -132,15 +165,15 @@ ALTER TABLE `users`
 ALTER TABLE `orders`
   MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+--
+-- AUTO_INCREMENT for table `vacancies`
+--
+ALTER TABLE `vacancies`
+  MODIFY `JobID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
